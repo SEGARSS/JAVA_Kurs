3150dcf5af388953e58430661d149eac1124dbc1
SEGARS <vim_branet@mail.ru>
2023-06-12 18:24:18 +0500

    дз2

--- Java_DZ/DZ_02/dz_02_00.java
+++ Java_DZ/DZ_02/dz_02_00.java
@@ -1,5 +1,11 @@
 package Java_DZ.DZ_02;
 
+/*Дана строка sql-запроса "select * from students where ". 
+Сформируйте часть WHERE этого запроса, используя StringBuilder. 
+Данные для фильтрации приведены ниже в виде json-строки.
+Если значение null, то параметр не должен попадать в запрос.
+Параметры для фильтрации: {"name":"Ivanov", "country":"Russia", "city":"Moscow", "age":"null"}*/
+
 public class dz_02_00 {
     public static void main(String[] args) throws Exception {
       String [] list = lib.ReadLineFromFile("D:\\Obuchenie\\JAVA_Kurs\\Java_DZ\\DZ_02\\dataForSelect.txt"); 
