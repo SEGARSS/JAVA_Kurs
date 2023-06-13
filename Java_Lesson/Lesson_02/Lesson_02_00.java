97bd106c0b8e84967e61d1005a5a44383d684b86
SEGARS <vim_branet@mail.ru>
2023-06-13 20:59:04 +0500

    лекция 2

---
+++ Java_Lesson/Lesson_02/Lesson_02_00.java
@@ -0,0 +1,33 @@
+package Lesson_02;
+
+/*Что такое API для нас: строки.*/
+public class Lesson_02_00 {
+    public static void main(String[] args) {
+        //Prim1();
+        Prim2();
+    }
+
+    private static void Prim1() {
+        var s = System.currentTimeMillis();
+        // String str = "";
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 1_000_000; i++) {
+            // str += "+";
+            sb.append("+");
+        }
+        System.out.println(System.currentTimeMillis() - s);
+        // System.out.println(str);
+        // System.out.println(sb);
+    }
+
+    private static void Prim2() {
+        String[] name = { "C", "е", "р", "г", "е", "й" };
+        String sk = "СЕРГЕЙ КА.";
+        System.out.println(sk.toLowerCase()); // сергей ка.
+        System.out.println(String.join("", name)); // Cергей
+        System.out.println(String.join("", "C", "е", "р", "г", "е", "й"));
+        // C,е,р,г,е,й
+        System.out.println(String.join(",", "C", "е", "р", "г", "е", "й"));
+    }
+
+}
---
+++ Java_Lesson/Lesson_02/Lesson_02_01.java
@@ -0,0 +1,20 @@
+package Lesson_02;
+
+import java.io.File;
+
+public class Lesson_02_01 {
+    public static void main(String[] args) {
+        String pathProject = System.getProperty("user.dir");
+        String pathDir = pathProject.concat("file.txt");
+        File dir = new File(pathDir);
+        System.out.println(dir.getAbsolutePath());
+        if (dir.mkdir()) {
+            System.out.println("+");
+        } else {
+            System.out.println("-");
+        }
+        for (String fname : dir.list()) {
+            System.out.println(fname);
+        }
+    }
+}
