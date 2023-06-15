1b0d4fd62df641118450fb6af112c2efdf63f4e3
SEGARS <vim_branet@mail.ru>
2023-06-15 14:24:10 +0500

    семинар 2

---
+++ Java_Seminar/Seminar3/task3.java
@@ -0,0 +1,24 @@
+package Java_Seminar.Seminar3;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
+
+public class task3 {
+    public static void ObjList() {
+        List<Object> oList = new ArrayList<>();
+        oList.add("a");
+        oList.add(1);
+        oList.add(2);
+        oList.add("b");
+        System.out.println(oList);
+
+        for (int i = 0; i < oList.size(); i++) {
+
+            System.out.println(oList.get(i).getClass());
+        }
+        oList.removeAll(Arrays.asList(new Integer[] { 1, 2 }));
+        System.out.println(oList);
+
+    }
+}
---
+++ Java_Seminar/Seminar_03.java
@@ -0,0 +1,13 @@
+package Java_Seminar;
+
+import Java_Seminar.Seminar3.*;
+
+public class Seminar_03 {
+    public static void main(String[] args) {
+        //task0.StringTest();
+        //task1.filler();
+        //task2.planetsFreq();
+        //task3.ObjList();
+        task4.task4();
+    }
+}
