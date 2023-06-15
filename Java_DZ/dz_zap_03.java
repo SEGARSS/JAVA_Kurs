9e4c1822845cc5a776544a200b7be7845a0c53ce
SEGARS <vim_branet@mail.ru>
2023-06-15 14:51:47 +0500

    дз3

---
+++ Java_DZ/dz_zap_03.java
@@ -0,0 +1,11 @@
+package Java_DZ;
+
+import Java_DZ.dz_03.*;
+
+public class dz_zap_03 {
+    public static void main(String[] args) throws Exception {
+        //task0.task0();
+        //task1.task1();
+        //task2.task2();
+    }
+}
