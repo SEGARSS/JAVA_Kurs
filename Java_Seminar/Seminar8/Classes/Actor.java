79c0144ec7540cc72b5605a175e859bf20a56da5
SEGARSS <vim_branet@mail.ru>
2023-07-03 11:15:49 +0500

    Семинар 8

---
+++ Java_Seminar/Seminar8/Classes/Actor.java
@@ -0,0 +1,31 @@
+package Java_Seminar.Seminar8.Classes;
+
+import Java_Seminar.Seminar8.Interfaces.iActorBehaviuor;
+
+public abstract class Actor implements iActorBehaviuor {
+    protected String name;
+    protected boolean isTakeOrder;
+    protected boolean isMakeOrder;
+
+    public Actor(String name) {
+        this.name = name;
+    }
+
+    abstract public String getName();
+
+    // public boolean isTakeOrder() {
+    //     return isTakeOrder;
+    // }
+
+    // public boolean isMakeOrder() {
+    //     return isMakeOrder;
+    // }
+
+    // public void setTakeOrder(boolean takeOrder) {
+    //     isTakeOrder = takeOrder;
+    // }
+
+    // public void setMakeOrder(boolean makeOrder) {
+    //     isMakeOrder = makeOrder;
+    // }
+}
---
+++ Java_Seminar/Seminar_08.java
@@ -0,0 +1,28 @@
+package Java_Seminar;
+
+import Java_Seminar.Seminar8.Classes.Actor;
+import Java_Seminar.Seminar8.Classes.Market;
+import Java_Seminar.Seminar8.Classes.OrdinaryClient;
+import Java_Seminar.Seminar8.Classes.PensionerClient;
+import Java_Seminar.Seminar8.Classes.SpecialClient;
+import Java_Seminar.Seminar8.Classes.TaxService;
+import Java_Seminar.Seminar8.Interfaces.iActorBehaviuor;
+
+public class Seminar_08 {
+    public static void main(String[] args) throws Exception {
+        //System.out.println("Hello, World!");
+        Market magnit = new Market();
+        iActorBehaviuor client1 = new OrdinaryClient("Boris");
+        iActorBehaviuor client2 = new SpecialClient("prezident",1001);
+        iActorBehaviuor p = new PensionerClient("Sergey Nikolay",1111);
+        iActorBehaviuor tax = new TaxService();
+
+        magnit.acceptToMarket(client1);
+        magnit.acceptToMarket(client2);
+        magnit.acceptToMarket(p);
+        magnit.acceptToMarket(tax);
+
+        magnit.update();
+    }
+}
+
